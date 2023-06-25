b769074eac127992a0db288a7f4f26c616b7c1dc
Blue <bluesillybeard2@gmail.com>
2023-06-24 21:31:14 -0600

    Optimized MeshBuilder and fixed a bug

--- MeshBuilder.cs
+++ MeshBuilder.cs
@@ -2,69 +2,104 @@ namespace vmodel;
 
 using System;
 using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Runtime.InteropServices;
 
 public sealed class MeshBuilder
 {
     //takes a hash code as an input, and outputs the index of the vertex with that hash code.
-    //TODO: sometimes there are hash collisions, deal with them.
-    private readonly Dictionary<int, uint> vertexLookup;
     private readonly List<float> vertices;
     private readonly List<uint> indices;
+    private readonly HashSet<int> vertexLookup; //To see if a vertex is already in the mesh somewhere
     public readonly Attributes attributes;
+    public readonly uint totalAttributes;
 
     public MeshBuilder(Attributes attributes)
     {
-        vertexLookup = new Dictionary<int, uint>();
         vertices = new List<float>();
         indices = new List<uint>();
         this.attributes = attributes;
+        totalAttributes = attributes.TotalAttributes();
+        vertexLookup = new HashSet<int>();
     }
     public MeshBuilder(Attributes attributes, int vertexCapacity, int indexCapacity)
     {
-        vertexLookup = new Dictionary<int, uint>(vertexCapacity);
         vertices = new List<float>(vertexCapacity);
-        indices = new List<uint>((int)(indexCapacity * attributes.TotalAttributes()));
+        indices = new List<uint>(indexCapacity);
         this.attributes = attributes;
+        totalAttributes = attributes.TotalAttributes();
+        vertexLookup = new HashSet<int>(vertexCapacity);
     }
 
     public void Clear()
     {
-        vertexLookup.Clear();
         vertices.Clear();
         indices.Clear();
     }
 
     public void AddVertex(params float[] vertex)
     {
-        if(vertex.Length != attributes.TotalAttributes())
+        if(vertex.Length != totalAttributes)
         {
             throw new Exception("Wrong attributes for vertex");
         }
         AddVertexInternal(vertex);
     }
 
-    private void AddVertexInternal(float[] vertex)
+    private void AddVertexInternal(ReadOnlySpan<float> vertex)
     {
         //If this vertex already exists, we just add it to our indices
-        int hash = GenerateHash(vertex);
-        if(vertexLookup.TryGetValue(hash, out var index))
+        if(FindVertex(vertex, out var index))
         {
             indices.Add(index);
             return;
         }
-        index = (uint)(vertices.Count / attributes.TotalAttributes());
+        index = (uint)(vertices.Count / totalAttributes);
         indices.Add(index);
-        vertices.AddRange(vertex);
-        vertexLookup.Add(hash, index);
+        //vertices.AddRange(vertex);
+        foreach(var val in vertex)
+        {
+            vertices.Add(val);
+        }
     }
 
+    private bool FindVertex(ReadOnlySpan<float> vertex, out uint index)
+    {
+        int hash = GenerateHash(vertex);
+        //If no vertex has a matching hash code, then we already know there are none
+        if(!vertexLookup.Contains(hash))
+        {
+            index = 0;
+            return false;
+        }
+        vertexLookup.Add(hash);
+        //Even if there is a matching hash code, that does not mean there is a matching vertex.
+        // So, search for the vertex to find the actual match
+        var numberOfVertices = vertices.Count / totalAttributes;
+        var verticesSpan = CollectionsMarshal.AsSpan(vertices);
+        for(uint i=0; i< numberOfVertices; i++)
+        {
+            //List.GetRange is slow, since it makes a copy.
+            // So, slicing a span is a lot faster.
+            var existingVertex = verticesSpan.Slice((int)(i*totalAttributes), (int)totalAttributes);
+            if(existingVertex.SequenceEqual(vertex))
+            {
+                index = i;
+                return true;
+            }
+        }
+        index = 0;
+        return false;
+    }
     public void AddVertex(int[] mapping, params float[] vertex)
     {
-        if(mapping.Length != attributes.TotalAttributes())
+        if(mapping.Length != totalAttributes)
         {
             throw new Exception("Mapping would not create correct attributes");
         }
-        AddVertexInternal(ConvertVertex(vertex, mapping));
+        Span<float> converted = stackalloc float[mapping.Length];
+        ConvertVertex(vertex, converted, mapping);
+        AddVertexInternal(converted);
     }
 
     public VMesh ToMesh()
@@ -79,7 +114,7 @@ public sealed class MeshBuilder
         return indices.Count % 3 != 0;
     }
 
-    private static int GenerateHash(float[] vertex){
+    private static int GenerateHash(ReadOnlySpan<float> vertex){
         HashCode hasher = new HashCode();
         foreach(float f in vertex){
            hasher.Add(BitConverter.SingleToInt32Bits(f));
@@ -102,105 +137,13 @@ public sealed class MeshBuilder
     {
         return ConvertVertex(vertex, mapping);
     }
+
+    public static void ConvertVertex(ReadOnlySpan<float> vertex, Span<float> destination, params int[] mapping){
+        for(int i=0; i<mapping.Length; i++){
+            int map = mapping[i];
+            if(map != -1){
+                destination[i] = vertex[map];
+            }
+        }
+    }
 }
-
-// 464955.4
-// public sealed class MeshBuilder
-// {
-//     //takes a hash code as an input, and outputs the index of the vertex with that hash code.
-//     private readonly Dictionary<int, uint> vertexLookup;
-//     private readonly List<float> vertices;
-//     private readonly List<uint> indices;
-//     public readonly Attributes attributes;
-
-//     public MeshBuilder(Attributes attributes)
-//     {
-//         vertexLookup = new Dictionary<int, uint>();
-//         vertices = new List<float>();
-//         indices = new List<uint>();
-//         this.attributes = attributes;
-//     }
-//     public MeshBuilder(Attributes attributes, int vertexCapacity, int indexCapacity)
-//     {
-//         vertexLookup = new Dictionary<int, uint>(vertexCapacity);
-//         vertices = new List<float>(vertexCapacity);
-//         indices = new List<uint>((int)(indexCapacity * attributes.TotalAttributes()));
-//         this.attributes = attributes;
-//     }
-
-//     public void Clear()
-//     {
-//         vertexLookup.Clear();
-//         vertices.Clear();
-//         indices.Clear();
-//     }
-
-//     public void AddVertex(params float[] vertex)
-//     {
-//         if(vertex.Length != attributes.TotalAttributes())
-//         {
-//             throw new Exception("Wrong attributes for vertex");
-//         }
-//         AddVertexInternal(vertex);
-//     }
-
-//     private void AddVertexInternal(float[] vertex)
-//     {
-//         //If this vertex already exists, we just add it to our indices
-//         int hash = GenerateHash(vertex);
-//         if(vertexLookup.TryGetValue(hash, out var index))
-//         {
-//             indices.Add(index);
-//             return;
-//         }
-//         index = (uint)(vertices.Count / attributes.TotalAttributes());
-//         indices.Add(index);
-//         vertices.AddRange(vertex);
-//         vertexLookup.Add(hash, index);
-//     }
-
-//     public void AddVertex(int[] mapping, params float[] vertex)
-//     {
-//         if(mapping.Length != attributes.TotalAttributes())
-//         {
-//             throw new Exception("Mapping would not create correct attributes");
-//         }
-//         AddVertexInternal(ConvertVertex(vertex, mapping));
-//     }
-
-//     public VMesh ToMesh()
-//     {
-//         uint[] indicesArr = indices.ToArray();
-//         float[] vertciesArr = vertices.ToArray();
-//         return new VMesh(vertciesArr, indicesArr, attributes, null);
-//     }
-
-//     public bool IsTriangular()
-//     {
-//         return indices.Count % 3 != 0;
-//     }
-
-//     private static int GenerateHash(float[] vertex){
-//         HashCode hasher = new HashCode();
-//         foreach(float f in vertex){
-//            hasher.Add(BitConverter.SingleToInt32Bits(f));
-//         }
-//         return hasher.ToHashCode();
-//     }
-
-//     public static float[] ConvertVertex(float[] vertex, params int[] mapping){
-//         float[] ret = new float[mapping.Length];
-//         for(int i=0; i<mapping.Length; i++){
-//             int map = mapping[i];
-//             if(map != -1){
-//                 ret[i] = vertex[map];
-//             }
-//         }
-//         return ret;
-//     }
-
-//     public static float[] ConvertVertex(int[] mapping, params float[] vertex)
-//     {
-//         return ConvertVertex(vertex, mapping);
-//     }
-// }
